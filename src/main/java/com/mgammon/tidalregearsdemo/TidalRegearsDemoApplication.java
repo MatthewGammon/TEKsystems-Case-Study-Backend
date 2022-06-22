a0c0c0916c3c61e8054d382613881febb0266f65
Matthew Gammon <mgammon91@gmail.com>
2022-06-22 16:44:10 -0400

    initial app file

---
+++ src/main/java/com/mgammon/tidalregearsdemo/TidalRegearsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mgammon.tidalregearsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidalRegearsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TidalRegearsDemoApplication.class, args);
+    }
+
+}
