3771fee170c8881919634726e7c77737c18a9f2e
Matthew Gammon <mgammon91@gmail.com>
2022-06-22 17:05:15 -0400

    committing the rest

---
+++ src/test/java/com/mgammon/tidalregearsdemo/TidalRegearsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgammon.tidalregearsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidalRegearsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
