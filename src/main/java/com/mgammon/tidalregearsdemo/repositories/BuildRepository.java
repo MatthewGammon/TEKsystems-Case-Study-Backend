3867ca9e680de5db46e1fb69ce19815a63e5beaa
Matthew Gammon <mgammon91@gmail.com>
2022-06-24 01:22:46 -0400

    create custom query and method to find a Build by all main gear pieces worn by a player

--- src/main/java/com/mgammon/tidalregearsdemo/repositories/BuildRepository.java
+++ src/main/java/com/mgammon/tidalregearsdemo/repositories/BuildRepository.java
@@ -2,8 +2,11 @@ package com.mgammon.tidalregearsdemo.repositories;
 
 import com.mgammon.tidalregearsdemo.models.Build;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BuildRepository extends JpaRepository<Build, Long> {
+    @Query(value = "SELECT b FROM Build b WHERE b.mainHand = ?1 and b.headGear = ?2 and b.chestGear = ?3 and b.shoes = ?4")
+    Build findBuildByRequiredGear(String mainHand, String headGear, String chestGear, String shoes);
 }
