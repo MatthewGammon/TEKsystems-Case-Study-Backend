5bb6f901b1367251c8b8d1b6a964ac844ecaeb90
Matthew Gammon <mgammon91@gmail.com>
2022-06-24 01:19:41 -0400

    add ExceptionHandlers for all newly created custom exceptions

--- src/main/java/com/mgammon/tidalregearsdemo/advice/MyControllerAdvice.java
+++ src/main/java/com/mgammon/tidalregearsdemo/advice/MyControllerAdvice.java
@@ -1,7 +1,6 @@
 package com.mgammon.tidalregearsdemo.advice;
 
-import com.mgammon.tidalregearsdemo.exceptions.InvalidPropertyTypeException;
-import com.mgammon.tidalregearsdemo.exceptions.NotFoundByIdException;
+import com.mgammon.tidalregearsdemo.exceptions.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +18,47 @@ public class MyControllerAdvice extends ResponseEntityExceptionHandler {
     public ResponseEntity<String> handleNotFoundById(NotFoundByIdException notFoundByIdException) {
         Long entityId = notFoundByIdException.getEntityId();
         String entityName = notFoundByIdException.getEntityName();
-        return new ResponseEntity<String>("No " + entityName + " found for the id: '" + entityId + "'.", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("No " + entityName + " found for the id: '" + entityId + "'.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<String> handleAlreadyExists(AlreadyExistsException alreadyExistsException) {
+        String eventId = alreadyExistsException.getEventId();
+        String entityName = alreadyExistsException.getEntityName();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Regear-Exists", "Regear Request already exists!");
+        return new ResponseEntity<>(entityName + " with an id of '" + eventId + "' already exists!", headers, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidPropertyTypeException.class)
     public ResponseEntity<String> handleInvalidPropertyType(InvalidPropertyTypeException invalidPropertyTypeException) {
         String propertyType = invalidPropertyTypeException.getPropertyType();
         String expectedType = invalidPropertyTypeException.getExpectedType();
-        return new ResponseEntity<String>("Property must be of type " + expectedType + " , but received " + propertyType + ".", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Property must be of type " + expectedType + " , but received " + propertyType + ".", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidBuildException.class)
+    public ResponseEntity<String> handleInvalidBuild(InvalidBuildException invalidBuildException) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Invalid-Build", "Invalid build. Please check the approved builds list.");
+        return new ResponseEntity<>("Invalid gear equipped! Please check the approved builds list.", headers, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ItemPowerTooLowException.class)
+    public ResponseEntity<String> handleLowItemPower(ItemPowerTooLowException itemPowerTooLowException) {
+        Integer itemPower = itemPowerTooLowException.getItemPower();
+        Integer minimumIp = itemPowerTooLowException.getMinimumIp();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Item-Power", "Item power is too low!");
+        return new ResponseEntity<>("Item power is too low! Your IP is " + itemPower + " and the minimum IP is " + minimumIp + ".", headers, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TierTooLowException.class)
+    public ResponseEntity<String> handleLowTier(TierTooLowException tierTooLowException) {
+        Integer minimumTierEquiv = tierTooLowException.getMinimumTierEquiv();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Tier-Equiv", "Items do not meet minimum tier.");
+        return new ResponseEntity<>("Item tier not high enough. Minimum tier equivalent is " + minimumTierEquiv + ".", headers, HttpStatus.BAD_REQUEST);
     }
 
 
