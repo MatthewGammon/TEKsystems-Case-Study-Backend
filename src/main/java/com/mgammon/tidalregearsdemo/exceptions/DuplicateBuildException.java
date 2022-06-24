02f42d9b3c93490aa115f1037ac881b24d95227a
Matthew Gammon <mgammon91@gmail.com>
2022-06-24 04:48:17 -0400

    create custom exception to handle a duplicate build being submitted

---
+++ src/main/java/com/mgammon/tidalregearsdemo/exceptions/DuplicateBuildException.java
@@ -0,0 +1,15 @@
+package com.mgammon.tidalregearsdemo.exceptions;
+
+import org.springframework.stereotype.Component;
+
+import java.io.Serial;
+
+
+@Component
+public class DuplicateBuildException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DuplicateBuildException() {
+    }
+}
