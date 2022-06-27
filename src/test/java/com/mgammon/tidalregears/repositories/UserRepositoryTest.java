5bb7a7f3a9e162fd0ab6b2b2e0cb2cca5089c661
Matthew Gammon <mgammon91@gmail.com>
2022-06-27 16:53:50 -0400

    add @EqualsandHashCode annotation

--- src/test/java/com/mgammon/tidalregears/repositories/UserRepositoryTest.java
+++ src/test/java/com/mgammon/tidalregears/repositories/UserRepositoryTest.java
@@ -1,2 +1,25 @@
-package com.mgammon.tidalregears.repositories;public class UserRepositoryTest {
+package com.mgammon.tidalregears.repositories;
+
+import com.mgammon.tidalregears.models.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void testFindByUsername() {
+        User user1 = new User("John", "john@gmail.com", "test123456");
+        userRepository.save(user1);
+        String user1name = user1.getUsername();
+        Optional<User> actualUser = userRepository.findByUsername(user1name);
+        assertEquals(user1, actualUser);
+    }
 }
